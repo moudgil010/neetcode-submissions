3129916768866248890e32ee4fbc02b26c8300a9
RAHUL SHARMA <159094482+moudgil010@users.noreply.github.com>
2026-07-24 23:20:22 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        map<int, int> freq;
+        for(int i=0;i<n;i++) {
+            freq[nums[i]]++;
+        }
+        for(const auto& num : freq) {
+            if(num.second > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
