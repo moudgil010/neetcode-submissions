f58899c6d3e9953678873b3198fd7499826e3f59
RAHUL SHARMA <159094482+moudgil010@users.noreply.github.com>
2026-07-24 16:59:59 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                if(nums[i]+nums[j] == target) {
+                    return {i,j};
+                }
+            }
+        }
+        // int i = 0;
+        // int j = n-1;
+        // while(i<=j) {
+        //     if(nums[i]+nums[j] == target) {
+        //         return {i,j};
+        //     }
+        //     else {
+        //         if(nums[i]+nums[j] < target) {
+        //             i++;
+        //         }
+        //         else j--;
+        //     }
+        // }
+        return {0};
+    }
+};
